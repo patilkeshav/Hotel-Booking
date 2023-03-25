df2894420385352071ecf88fc6676ce18ce9f5c0
patilkeshav <patilkeshav66@gmail.com>
2023-03-25 23:00:40 +0530

    pacakes_implementation

--- src/app/modules/customer/hotel-preferences/hotel-preferences.component.ts
+++ src/app/modules/customer/hotel-preferences/hotel-preferences.component.ts
@@ -6,24 +6,43 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './hotel-preferences.component.html',
   styleUrls: ['./hotel-preferences.component.scss']
 })
-export class HotelPreferencesComponent implements OnInit{
-  @Input() 
-  hotelList:any=[];
+export class HotelPreferencesComponent implements OnInit {
+  // getLocationData: any;
+  filterData:any=[];
+  packageName:any=[];
+
+  @Input()
   getLocationData:any=[];
-  constructor(private http:HttpService){
+
+  constructor(private http: HttpService) {
   }
-  
+
   ngOnInit(): void {
-    this. getLocations();
+    this.getLocations();
   }
-  getLocations(){
-    this.http.getDataFromServer("hotelLocations").subscribe((responce:any)=>{
-      if(responce && responce.length){
-           this.getLocationData=responce;
-           console.log("getlocation",this.getLocationData)}
+  getLocations() {
+    this.http.getDataFromServer("hotelLocations").subscribe((responce: any) => {
+      if (responce && responce.length) {
+        this.getLocationData = responce;
+        console.log("getlocation", this.getLocationData)
+      }
     },
-    error=>{
-      console.log(error)
-    })
-    }
+      error => {
+        console.log(error)
+      })
+  }
+//   getPackagesName(){
+//     this.http.getDataFromServer("packages").subscribe((el:any)=>{
+//       if(el && el.length>0){
+//        this.packageName=el;
+//        console.log("packageName",this.packageName)
+//       }
+//     })
+//    }
+//   filteredData(hotelPackages:any){
+//     this.filterData=this.getLocationData.map((el:any)=>el.packageType.filter((el:any)=>el.name===hotelPackages.name))
+//     console.log("filterData",this.filterData)
+//   this.hotelList.push(this.filterData)}
+  
+// }
 }
--- src/app/modules/customer/landing-page/landing-page.component.ts
+++ src/app/modules/customer/landing-page/landing-page.component.ts
@@ -9,12 +9,28 @@ import { HttpService } from 'src/app/services/http.service';
 export class LandingPageComponent implements OnInit {
  packageName:any=[]
  selectedPackages:any;
- getLocationData:any=[]
+ getLocationData:any=[];
+ hotelLocations:any=[];
+ filterData:any=[];
+ showLocation:boolean=false;
+ showAlllocations:boolean=true;
   constructor(private http:HttpService,){}
  ngOnInit(): void {
   this.getPackagesName();
+  this.getHotelLocations()
   
  }
+ getHotelLocations() {
+  this.http.getDataFromServer("hotelLocations").subscribe((responce: any) => {
+    if (responce && responce.length) {
+      this.getLocationData = responce;
+      console.log("getlocation", this.getLocationData)
+    }
+  },
+    error => {
+      console.log(error)
+    })
+}
  getPackagesName(){
   this.http.getDataFromServer("packages").subscribe((el:any)=>{
     if(el && el.length>0){
@@ -24,11 +40,36 @@ export class LandingPageComponent implements OnInit {
   })
  }
  showPackages(hotelPackages:any){
-    this.selectedPackages=hotelPackages;
-    console.log("selected packages",hotelPackages)
-    
- 
 
+  const tempArr:any=[];
+  for(let i=0;i<this.getLocationData.length;i++){
+    const item=this.getLocationData[i];
+    const data=item.packageType.filter((el:any)=>el.name ==hotelPackages.name);
+    if(data && data.length>0){
+      tempArr.push(item);
+      this.filterData=tempArr;
+      console.log("filterData",tempArr)
+      this.showLocation=true;
+      this. showAlllocations=false;
+    }
+   
+    
+
+  }
+//    let produArr:any[]=[];
+//     this.selectedPackages=hotelPackages;
+//     this.showLocation=true
+//    
+//     console.log("selected packages",hotelPackages)
+//  this.filterData=this.getLocationData.map((el:any)=>el.packageType.filter((el:any)=>el.name===hotelPackages.name))
+//  if(this.filterData){
+//   console.log("product",this.filterData)
+//  }
+
+     
  }
 
-}
+ }
+ 
+ 
+
